5d7f0e52adc772ec29784783ff357d5d653e7491
mirucon <mirucons@gmail.com>
2017-06-04 22:58:34 +1200

    Add movement of header paddings and fix some bugs

--- page.php
+++ page.php
@@ -21,18 +21,21 @@
 
             <div class="content-inner">
 
-              <div class="entry content-box">
+              <div class="content-inside">
 
-                <div class="post-meta">
+                <div class="post-meta content-box">
                   <?php if ( cd_is_meta_date() ){ ?><span class="post-date"><?php echo get_the_date(); ?></span><?php } ?>
                   <?php if ( cd_is_meta_author() ){ ?><span class="post-author"><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php echo $author = get_the_author(); ?></a></span><?php } ?>
                   <?php if ( cd_is_meta_com() && comments_open() && cd_is_post_single_comment() ) { ?><span class="post-comment"><?php comments_popup_link('0', '1', '%'); ?></span><?php } ?>
                 </div>
-                <div class="entry-inner"><?php the_content(); ?></div>
+                <div class="entry">
+                  <div class="entry-inner content-box"><?php the_content(); ?></div>
+                </div>
                 <?php wp_link_pages( $defaults = array( 'before' => '<div class="post-pages">' . __( 'Pages:', 'coldbox' ), 'after'=> '</div>', 'link_before' => '<span class="page-number">', 'link_after' => '</span>' ) ); ?>
-              </div>
 
-              <?php if ( cd_is_post_single_comment() ) { comments_template( '/comments.php', true ); } ?>
+                <?php if ( cd_is_post_single_comment() ) { comments_template( '/comments.php', true ); } ?>
+
+              </div><!--/.content-inside-->
 
             </div><!--/.content-inner-->
           </div><!--/.content-->
