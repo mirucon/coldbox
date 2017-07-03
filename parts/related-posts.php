4c060312c397966a9acd77c3d8b02a16ea5fd1e9
mirucon <mirucons@gmail.com>
2017-07-03 21:23:31 +1200

    To use both tags and categories to get related posts

--- parts/related-posts.php
+++ parts/related-posts.php
@@ -2,25 +2,56 @@
 /* ------------------------------------------------------------------------- *
 * Get Related Posts
 * ------------------------------------------------------------------------- */
-$categories = get_the_category($post->ID);
-$category_ID = array();
-foreach ( $categories as $category ) {
-  array_push( $category_ID, $category -> cat_ID );
+$max_articles = cd_single_related_max();
+
+$tags = wp_get_post_tags($post -> ID);
+$tag_ids = array();
+
+foreach ( $tags as $tag ) {
+  array_push( $tag_ids, $tag -> term_id);
 }
-$args = array(
+
+$tag_args = array(
   'post__not_in' => array($post -> ID),
-  'posts_per_page' => cd_single_related_max(), // You can change this value on the theme customizer.
-  'category__in' => $category_ID,
+  'posts_per_page'=> $max_articles,
+  'tag__in' => $tag_ids,
   'orderby' => 'rand',
 );
-$query = new WP_Query( $args ); ?>
 
-<?php if ( $query -> have_posts() ): ?>
+$related_posts = get_posts( $tag_args );
+$posts_by_cats_count = count( $related_posts );
+
+// If hasn't got enought by tags, also gets by categories
+if ( $max_articles > $posts_by_cats_count || empty( $tags ) ) {
+
+  $categories = get_the_category( $post -> ID );
+  $category_ID = array();
+
+  foreach ( $categories as $category ) {
+    array_push( $category_ID, $category -> cat_ID);
+  }
+
+  $cat_args = array(
+    'post__not_in' => array( $post -> ID ),
+    'posts_per_page'=> ( $max_articles - $posts_by_cats_count ),
+    'category__in' => $category_ID,
+    'orderby' => 'rand',
+  );
+  $posts_by_cats = get_posts( $cat_args );
+
+  $related_posts = array_merge( $related_posts, $posts_by_cats );
+} ?>
+
+
+<?php if ( count( $related_posts ) > 0 ) : ?>
+
   <section class="related-posts content-box">
 
     <h4 class="related-head"><?php esc_html_e( 'Related Posts', 'coldbox' ); ?></h4>
     <ul class="related-posts-list">
-      <?php while ( $query -> have_posts() ): $query -> the_post(); ?>
+
+
+      <?php foreach ( $related_posts as $post ) : setup_postdata( $post ); ?>
 
         <li class="related-article">
           <article <?php post_class();?>>
@@ -28,7 +59,7 @@ $query = new WP_Query( $args ); ?>
             <figure class="post-thumbnail">
               <a href="<?php the_permalink(); ?>">
                 <?php if ( has_post_thumbnail() ): ?>
-                  <?php the_post_thumbnail('cd-standard'); ?>
+                  <?php the_post_thumbnail( 'cd-standard' ); ?>
                 <?php else: ?>
                   <img src="<?php echo esc_attr( get_template_directory_uri() . '/img/thumb-standard.png' ); ?>" alt="noimage" height="500" width="500">
                 <?php endif; ?>
@@ -43,10 +74,12 @@ $query = new WP_Query( $args ); ?>
           </article>
         </li>
 
-      <?php endwhile;?>
+      <?php endforeach; ?>
+
     </ul>
 
   </section>
+
 <?php endif; ?>
 
 <?php wp_reset_postdata(); ?>
