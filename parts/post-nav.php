666bdd29a37f800ba55b358d0edd225310e0b8b2
mirucon <mirucons@gmail.com>
2017-07-27 22:45:37 +1200

    Polish

--- parts/post-nav.php
+++ parts/post-nav.php
@@ -13,24 +13,33 @@
 	<ul>
 		<?php
 		$prev_post = get_previous_post();
+
 		if ( $prev_post ) :
-			$prevthumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $prev_post -> ID ), array( 600, 600 ), false, '' ); ?>
-			<?php if ( has_post_thumbnail( $prev_post ) ) : ?>
+
+			$prevthumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $prev_post -> ID ), array( 600, 600 ), false, '' );
+
+			if ( has_post_thumbnail( $prev_post ) ) : ?>
 				<li class="prev"><?php previous_post_link( '%link', '<div class="post-thumbnail" style="background-image: url( ' . $prevthumbnail[0] . ' )"></div> <span class="chevron-left" aria-hidden="true"></span> <p class="nav-title">' . __( 'Prev Post', 'coldbox' ) . '</p> <p class="post-title">%title</p>' ); ?></li>
 			<?php else : ?>
 				<li class="prev"><?php previous_post_link( '%link', '<span class="chevron-left" aria-hidden="true"></span> <p class="nav-title">' . __( 'Prev Post', 'coldbox' ) . '</p> <p class="post-title">%title</p>' ); ?> </li>
 			<?php endif; ?>
+
 		<?php endif;
 
 		$next_post = get_next_post();
+
 		if ( $next_post ) :
+
 			$nextthumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $next_post -> ID ), array( 600, 600 ), false, '' );
+
 			if ( has_post_thumbnail( $next_post ) ) : ?>
 				<li class="next"><?php next_post_link( '%link', '<div class="post-thumbnail" style="background-image: url( ' . $nextthumbnail[0] . ' )"></div> <span class="chevron-right" aria-hidden="true"></span> <p class="nav-title">' . __( 'Next Post', 'coldbox' ) . '</p> <p class="post-title">%title</p>' ); ?></li>
 			<?php else : ?>
 				<li class="next"><?php next_post_link( '%link', '<span class="chevron-right" aria-hidden="true"></span> <p class="nav-title">' . __( 'Next Post', 'coldbox' ) . '</p> <p class="post-title">%title</p>' ); ?></li>
 			<?php endif; ?>
+
 		<?php endif; ?>
+
 	</ul>
 
 </nav>
