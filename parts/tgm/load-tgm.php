cec17c770e9d13e100382fcb27a5527d1085d22c
mirucon <mirucons@gmail.com>
2017-08-09 21:27:29 +1200

    Fixed: Loading TGM

--- parts/tgm/load-tgm.php
+++ parts/tgm/load-tgm.php
@@ -31,7 +31,7 @@
  * Plugin:
  * require_once dirname( __FILE__ ) . '/path/to/class-tgm-plugin-activation.php';
  */
-require_once get_template_directory() . '/parts/tgm/class-tgm-plugin-activation.php';
+get_template_part( 'parts/tgm/class-tgm-plugin-activation' );
 
 add_action( 'tgmpa_register', 'cd_register_required_plugins' );
 
