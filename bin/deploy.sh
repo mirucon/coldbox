fef77f205a07f08ef8410bf8a083a75494857e4e
mirucon <i@miruc.co>
2017-11-08 08:40:23 +1300

    Updated: Debug tasks

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,7 +6,7 @@ if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
     echo "Not deploying pull requests."
     exit
 fi
- 
+
 if [[ "master" != "$TRAVIS_BRANCH" ]]; then
     echo "Not on the 'master' branch."
     exit
