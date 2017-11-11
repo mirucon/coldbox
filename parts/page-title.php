d4b0e2ccde0872aef248fbb6a546ebbd8f6edbd6
mirucon <i@miruc.co>
2017-11-11 17:44:39 +1300

    Improved: The way to get term description for archive pages

--- parts/page-title.php
+++ parts/page-title.php
@@ -8,11 +8,7 @@
 
 if ( is_category() ) : ?>
 	<?php the_archive_title(); ?>
-	<?php if ( term_description() !== '' ) : ?>
-		<div class="taxonomy-description">
-			<?php echo term_description(); ?>
-		</div>
-	<?php endif; ?>
+	<?php the_archive_description( '<div class="taxonomy-description">', '</div>' ); ?>
 
 
 <?php elseif ( is_tag() ) : ?>
@@ -26,9 +22,9 @@ if ( is_category() ) : ?>
 
 <?php elseif ( is_search() ) : ?>
 	<h1><span class="title-description"><?php esc_html_e( 'Search Results for:', 'coldbox' ); ?>&#32;</span><?php the_search_query(); ?></h1>
-		<div class="taxonomy-description">
-			<?php get_search_form(); ?>
-		</div>
+	<div class="taxonomy-description">
+		<?php get_search_form(); ?>
+	</div>
 
 
 <?php elseif ( is_day() ) : ?>
@@ -45,14 +41,13 @@ if ( is_category() ) : ?>
 
 <?php elseif ( is_author() ) : ?>
 	<?php the_archive_title(); ?>
-	<?php if ( strlen( the_author_meta( 'description' ) ) > 0 ) : ?>
-		<div class="taxonomy-description">
-			<?php the_author_meta( 'description' ); ?>
-		</div>
-	<?php endif; ?>
+	<?php the_archive_description( '<div class="taxonomy-description">', '</div>' ); ?>
 
 
 <?php elseif ( is_404() ) : ?>
-	<h1><span class="title-description"><?php esc_html_e( 'ERROR 404.', 'coldbox' ); ?>&#32;</span><?php esc_html_e( 'Sorry, Page Not Found.', 'coldbox' ); ?></h1>
+	<h1>
+		<span class="title-description"><?php esc_html_e( 'ERROR 404.', 'coldbox' ); ?>&#32;</span>
+		<?php esc_html_e( 'Sorry, Page Not Found.', 'coldbox' ); ?>
+	</h1>
 
-	<?php endif; ?>
+<?php endif; ?>
