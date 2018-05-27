fc185520fe1d666295db045ee6aadb7df4ce1a87
mirucon <i@miruc.co>
2018-05-27 14:17:12 +0900

    Add: Link to privacy policy page in footer

--- footer.php
+++ footer.php
@@ -42,6 +42,11 @@ else :
 					?>
 
 					<?php echo wp_kses( cd_credit(), $allowed_html ); ?>
+
+					<?php if ( function_exists( 'the_privacy_policy_link' ) ) : ?>
+						<?php the_privacy_policy_link( ' / ', '<span role="separator" aria-hidden="true"></span>' ); ?>
+					<?php endif; ?>
+
 				</p>
 
 				<?php if ( cd_is_theme_credit() ) : ?>
