1798c0a5de93e4c6c210c1be99afcc502ea63cf6
Mirucon <i@miruc.co>
2018-06-27 00:43:39 +0000

    Update from travis dabf509fa79f7ca4bcb0340e1789610efdf3f1de

--- parts/admin-notices.php
+++ parts/admin-notices.php
@@ -34,15 +34,17 @@ function cd_admin_notice_ads_extension() {
 					/* translators: 1: opening a tag, 2: closing a tag */
 					esc_html__( 'Thanks for using the Coldbox theme! We have released an extension plugin called Coldbox Ads Extension for the easy Google AdSense management and getting high revenue with it.
 					We believe this is the best choice to monetize your blog with the Coldbox theme. %1$sSee this article%2$s for more details.', 'coldbox' ),
-					'<a href="' . esc_url( __( 'https://coldbox.miruc.co/addons/google-adsense-extension/' ) ) . '">',
+					'<a href="' . esc_url( __( 'https://coldbox.miruc.co/addons/google-adsense-extension/', 'coldbox' ) ) . '">',
 					'</a>'
 				)
 			?>
 		</p>
 		<p>
+			<a href="<?php echo esc_url( __( 'https://coldbox.miruc.co/addons/google-adsense-extension/', 'coldbox' ) ); ?>">
+				<?php esc_html_e( 'See more about the extension', 'coldbox' ); ?></a>
+			<span> | </span>
 			<a href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'coldbox-dismiss-ads', 'dismiss_admin_notice' ), 'coldbox-dismiss-ads-' . get_current_user_id() ) ); ?>" target="_parent">
-				<?php esc_html_e( 'Dismiss this notice', 'coldbox' ); ?>
-			</a>
+				<?php esc_html_e( 'Dismiss this notice', 'coldbox' ); ?></a>
 		</p>
 	</div>
 	<?php
