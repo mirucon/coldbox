4909e05557f9809a3a01fbdf8bd00711283c412b
mirucon <i@miruc.co>
2018-06-28 22:37:30 +0900

    Fix deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,10 +11,16 @@ if [[ "master" != "$TRAVIS_BRANCH" ]]; then
     echo "Not on the 'master' branch."
     exit
 fi
+
+if [[ "7.2" != "$TRAVIS_PHP_VERSION" ]]; then
+    echo "Deploy only works on PHP 7.2."
+    exit
+fi
  
 git clone -b dist --quiet "https://github.com/${TRAVIS_REPO_SLUG}.git" dist
 npm run build
 cd dist
+ls -la
 git add -A
 git commit -m "Update from travis $TRAVIS_COMMIT"
 git push --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" dist 2> /dev/null
