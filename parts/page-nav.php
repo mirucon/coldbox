c356c80c78f274c75ecc31c4162feed56d273221
mirucon <i@miruc.co>
2018-06-29 09:26:10 +0900

    chore: Refactoring code and security things

--- parts/page-nav.php
+++ parts/page-nav.php
@@ -8,7 +8,7 @@
 
 global $wp_query;
 
-$paginate_links = the_posts_pagination(
+the_posts_pagination(
 	array(
 		'type'              => 'list',
 		'end_size'          => '2',
@@ -19,32 +19,3 @@ $paginate_links = the_posts_pagination(
 		'after_page_number' => '',
 	)
 );
-
-$allowed_html = array(
-	'a'      => array(
-		'href'    => array(),
-		'onclick' => array(),
-		'target'  => array(),
-		'class'   => array(),
-	),
-	'p'      => array(
-		'style'  => array(),
-		'align'  => array(),
-		'target' => array(),
-		'class'  => array(),
-	),
-	'br'     => array(),
-	'strong' => array(),
-	'b'      => array(),
-	'ul'     => array(
-		'class' => array(),
-	),
-	'li'     => array(
-		'class' => array(),
-	),
-	'span'   => array(
-		'class' => array(),
-	),
-);
-
-echo wp_kses( $paginate_links, $allowed_html );
