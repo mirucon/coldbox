6813a1eaf59544a734ec5bd7564400a8bb8e8dfb
mirucon <i@miruc.co>
2018-06-30 21:55:56 +0900

    Add corresponding labels for forms

--- searchform.php
+++ searchform.php
@@ -9,7 +9,8 @@
 ?>
 
 <form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="search" class="search-inner" name="s" placeholder="<?php esc_attr_e( 'Search this site', 'coldbox' ); ?>" />
+	<label for="s" class="screen-reader-text"><?php esc_html_e( 'Search', 'coldbox' ); ?></label>
+	<input type="search" class="search-inner" name="s" placeholder="<?php esc_attr_e( 'Search this site', 'coldbox' ); ?>" value="<?php echo esc_attr( trim( get_search_query() ) ); ?>"/>
 	<button type="submit" class="search-submit">
 		<span class="icon search"></span>
 		<span class="screen-reader-text"><?php esc_html_e( 'Search', 'coldbox' ); ?></span>
