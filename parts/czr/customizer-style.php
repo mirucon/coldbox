8124270af597d3266108ada2e7a8df2c6c42b06d
Toshihiro Kanai <i@miruc.co>
2018-07-26 10:47:37 +0900

    refactor: Improve readability of customizer style

--- parts/czr/customizer-style.php
+++ parts/czr/customizer-style.php
@@ -38,7 +38,11 @@ if ( ! function_exists( 'cd_customizer_style' ) ) {
 		// Font size for mobile devices.
 		if ( get_theme_mod( 'global_font_size_mobile', 15 ) !== 15 ) {
 			$font_size_mobile = get_theme_mod( 'global_font_size_mobile' );
-			$czr_style       .= "@media screen and ( max-width: 767px ) { body { font-size: ${font_size_mobile}px; } } ";
+			$czr_style       .= "@media screen and ( max-width: 767px ) {
+				body {
+					font-size: ${font_size_mobile}px;
+				}
+			}";
 		}
 
 		/*
@@ -47,13 +51,17 @@ if ( ! function_exists( 'cd_customizer_style' ) ) {
 		 * -------------------------------------------------------------------------
 		 */
 
+		// Whether to hide site title and description.
 		if ( ! display_header_text() ) {
 			$czr_style .= '.site-title, .site-description {
-			   position: absolute; width: 1px; height: 1px; clip: rect(1px, 1px, 1px, 1px); overflow: hidden;
+			   position: absolute;
+			   width: 1px; height: 1px;
+			   clip: rect(1px, 1px, 1px, 1px);
+			   overflow: hidden;
 			}';
 		}
 
-		// Add decorations to h tags.
+		// Whether to add decoration to headings.
 		if ( get_theme_mod( 'decorate_htags', false ) ) {
 			$czr_style .= '.entry h2 { margin: 2em -40px 1.3em; padding: 1.3rem 30px; border-style: solid; border-width: 1px 0; overflow: hidden; }
 			@media screen and (max-width: 640px) { .entry h2 { margin-left: -20px; margin-right: -20px; padding-left: 10px; padding-right: 10px; } }
@@ -62,42 +70,45 @@ if ( ! function_exists( 'cd_customizer_style' ) ) {
 			.entry h5 { padding: 0 0 .4rem; border-bottom: 1px dotted #bbb; overflow: hidden; }';
 		}
 
-		// Change grid columns for desktop & tablet.
+		// Change the number of grid columns for desktop & tablet.
 		if ( get_theme_mod( 'grid_columns', 2 ) !== 2 ) {
-			$grid_columns   = absint( get_theme_mod( 'grid_columns' ) );
+			$grid_columns   = absint( get_theme_mod( 'grid_columns', 2 ) );
 			$width_percents = 100 / $grid_columns . '%';
 			$czr_style     .= ".grid-view .article{width: $width_percents;}";
 		}
 
-		// Change grid columns for mobile.
+		// Change the number of grid columns for mobile.
 		if ( get_theme_mod( 'grid_columns_mobile', 1 ) !== 1 ) {
 			$grid_columns   = absint( get_theme_mod( 'grid_columns_mobile', 1 ) );
 			$width_percents = 100 / $grid_columns . '%';
 			$czr_style     .= "@media screen and (max-width: 640px){body .container .grid-view .article{width: $width_percents;}}";
 		}
 
-		// Related Posts Columns.
+		// Related posts the number of columns.
 		if ( get_theme_mod( 'single_related_col', 3 ) !== 3 ) {
-			$rel_col    = esc_html( get_theme_mod( 'single_related_col' ) );
+			$rel_col    = esc_html( get_theme_mod( 'single_related_col', 3 ) );
 			$czr_style .= " .related-posts .related-article { width: calc(100% / {$rel_col}); } ";
 		}
 
-		// Change .site-info Padding.
-		if ( get_theme_mod( 'header_padding', '30' ) !== '30' ) {
-			$header_padding = absint( get_theme_mod( 'header_padding' ) );
+		// Change .site-info padding.
+		if ( get_theme_mod( 'header_padding', 30 ) !== 30 ) {
+			$header_padding = absint( get_theme_mod( 'header_padding', 30 ) );
 			$czr_style     .= "body .site-info { padding-top:${header_padding}px; padding-bottom:${header_padding}px }";
 		}
 
-		// Whether to use narrower padding.
+		// Whether to use narrower padding when scrolling.
 		if ( ! get_theme_mod( 'use_narrower_padding', true ) ) {
-			$header_padding = absint( get_theme_mod( 'header_padding', '30' ) );
-			$czr_style     .= "body.header-row .sticky .site-info { padding-top:${header_padding}px; padding-bottom:${header_padding}px }";
+			$header_padding = absint( get_theme_mod( 'header_padding', 30 ) );
+			$czr_style     .= "body.header-row .sticky .site-info {
+				padding-top: ${header_padding}px;
+				padding-bottom: ${header_padding}px;
+			}";
 		}
 
 		// Custom Logo Sizing.
-		if ( get_theme_mod( 'logo_width', '230' ) !== '230' ) {
-			$logo_width = esc_html( get_theme_mod( 'logo_width' ) );
-			$czr_style .= ".site-info img{max-width:${logo_width}px}";
+		if ( get_theme_mod( 'logo_width', 230 ) !== 230 ) {
+			$logo_width = esc_html( get_theme_mod( 'logo_width', 230 ) );
+			$czr_style .= ".site-info img { max-width: ${logo_width}px }";
 		}
 
 		/*
@@ -109,18 +120,33 @@ if ( ! function_exists( 'cd_customizer_style' ) ) {
 		// Link Color.
 		if ( get_theme_mod( 'link_color', '#00619f' ) !== '#00619f' ) {
 			$color_link = esc_html( get_theme_mod( 'link_color' ) );
-			$czr_style .= ".entry a, .title-box a:hover, .post-meta a:hover, .post-meta.content-box a:hover, .post-btm-tags a:hover, p.post-btm-cats a:hover, .related-posts .post-category a, .related-posts .post:hover .post-title, .post-pages, .grid-view .post-inner a:hover .post-title, .standard-view .post-title:hover, ul.page-numbers, .widget #wp-calendar a, .widget .widgets-list-layout li:hover a, #comment-list .comment-author .fn a, #respond .logged-in-as a:hover, .comment-pages, .comment-pages a,.comment-pages span, .comment-body a, .comment-tabmenu .active > a, .standard-view .post-inner:hover .post-title, .widget .textwidget a { color: $color_link; }
-			#comments input[type=submit], .post-tags a, .post-tags a, .main-archive .post-date, .action-bar, input[type=submit]:hover, input[type=submit]:focus, input[type=button]:hover, input[type=button]:focus, button[type=submit]:hover, button[type=submit]:focus, button[type=button]:hover, button[type=button]:focus { background-color: $color_link; }
-			textarea:focus { border-color: $color_link; }
-			.comment-pages > a:hover, .comment-pages span, .post-pages > a:hover>span,.post-pages>span, ul.page-numbers span.page-numbers.current, ul.page-numbers a.page-numbers:hover { border-bottom-color: $color_link; }
-			::selection { background-color: $color_link; }
-			::moz-selection { background-color: $color_link; }";
+			$czr_style .= ".entry a, .title-box a:hover, .post-meta a:hover, .post-meta.content-box a:hover, .post-btm-tags a:hover, p.post-btm-cats a:hover,
+			.related-posts .post-category a, .related-posts .post:hover .post-title, .post-pages, .grid-view .post-inner a:hover .post-title,
+			.standard-view .post-title:hover, ul.page-numbers, .widget #wp-calendar a, .widget .widgets-list-layout li:hover a,
+			#comment-list .comment-author .fn a, #respond .logged-in-as a:hover, .comment-pages, .comment-pages a,.comment-pages span,
+			.comment-body a, .comment-tabmenu .active > a, .standard-view .post-inner:hover .post-title, .widget .textwidget a {
+				color: ${color_link};
+			}
+			#comments input[type=submit], .post-tags a, .post-tags a, .main-archive .post-date, .action-bar,input[type=submit]:hover,
+			input[type=submit]:focus, input[type=button]:hover, input[type=button]:focus, button[type=submit]:hover, button[type=submit]:focus,
+			button[type=button]:hover, button[type=button]:focus {
+				background-color: ${color_link};
+			}
+			.comment-pages > a:hover, .comment-pages span, .post-pages > a:hover>span,.post-pages>span,
+			ul.page-numbers span.page-numbers.current,ul.page-numbers a.page-numbers:hover {
+				border-bottom-color: ${color_link};
+			}
+			textarea:focus { border-color: ${color_link}; }
+			::selection { background-color: ${color_link}; }
+			::moz-selection { background-color: ${color_link}; }";
 		}
 
 		// Link Hover Color.
 		if ( get_theme_mod( 'link_hover_color', '#2e4453' ) !== '#2e4453' ) {
 			$color_hover = esc_html( get_theme_mod( 'link_hover_color' ) );
-			$czr_style  .= ".entry a:hover, .comment-body a:hover, .sidebar #wp-calender a:hover, .widget .textwidget a:hover { color: $color_hover; }";
+			$czr_style  .= ".entry a:hover, .comment-body a:hover, .sidebar #wp-calender a:hover, .widget .textwidget a:hover {
+				color: ${color_hover};
+			}";
 		}
 
 		// Header text color.
@@ -139,12 +165,14 @@ if ( ! function_exists( 'cd_customizer_style' ) ) {
 			$r                 = $r > 255 ? 255 : $r;
 			$g                 = $g > 255 ? 255 : $g;
 			$b                 = $b > 255 ? 255 : $b;
-			$czr_style        .= ".site-info,.site-title{color:#${header_textcolor};}
-			.site-description{color:rgb(${r}, ${g}, ${b});}
-			.header-menu .menu-container > li > a{color:#${header_textcolor}}
-			.header-menu .menu-container > li:hover > a{color:rgb(${r_h}, ${g_h}, ${b_h})}
-			:root body .search-toggle span.icon.search{border-color:#${header_textcolor}}
-			:root body .search-toggle span.icon.search::before,.nav-toggle .bottom, .nav-toggle .middle, .nav-toggle .top{background-color:#${header_textcolor}}";
+			$czr_style        .= ".site-info,.site-title { color: #${header_textcolor}; }
+			.site-description{ color: rgb(${r}, ${g}, ${b}); }
+			.header-menu .menu-container > li > a { color: #${header_textcolor} }
+			.header-menu .menu-container > li:hover > a { color: rgb(${r_h}, ${g_h}, ${b_h}) }
+			:root body .search-toggle span.icon.search { border-color: #${header_textcolor} }
+			:root body .search-toggle span.icon.search::before,.nav-toggle .bottom, .nav-toggle .middle, .nav-toggle .top {
+				background-color: #${header_textcolor}
+			}";
 		}
 
 		// Header Background Color.
@@ -166,7 +194,11 @@ if ( ! function_exists( 'cd_customizer_style' ) ) {
 		// Header Menu Background Color for Mobile devices.
 		if ( get_theme_mod( 'header_menu_background', '#51575d' ) !== '#51575d' ) {
 			$header_menu_background = esc_html( get_theme_mod( 'header_menu_background' ) );
-			$czr_style             .= "@media screen and (max-width: 767px){#header-nav{background-color:${header_menu_background}}}";
+			$czr_style             .= "@media screen and (max-width: 767px) {
+				#header-nav { 
+					background-color:${header_menu_background}
+				}
+			}";
 		}
 
 		// Footer Menu Background Color.
