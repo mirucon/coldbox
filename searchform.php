017a75321e115aca17edf23fba254eb6650b03fb
Toshihiro <i@miruc.co>
2018-08-08 12:56:09 +0000

    Update from travis 469dd79ebc89b7cf8ee42e3c1b109db5bb8a6ce7

--- searchform.php
+++ searchform.php
@@ -11,7 +11,7 @@ $id = 'search-form' . wp_rand( 1, 3 );
 
 <form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 	<label for="<?php echo esc_attr( $id ); ?>" class="screen-reader-text"><?php esc_html_e( 'Search', 'coldbox' ); ?></label>
-	<input type="search" class="search-inner" name="search-inner" id="<?php echo esc_attr( $id ); ?>" placeholder="<?php esc_attr_e( 'Search this site', 'coldbox' ); ?>" value="<?php echo esc_attr( trim( get_search_query() ) ); ?>"/>
+	<input type="search" class="search-inner" name="s" id="<?php echo esc_attr( $id ); ?>" placeholder="<?php esc_attr_e( 'Search this site', 'coldbox' ); ?>" value="<?php echo esc_attr( trim( get_search_query() ) ); ?>"/>
 	<button type="submit" class="search-submit">
 		<span class="icon search" aria-hidden="true"></span>
 		<span class="screen-reader-text"><?php esc_html_e( 'Search', 'coldbox' ); ?></span>
