970143536d11597539fe7fccfff38e1c7d644518
Toshihiro Kanai <i@miruc.co>
2018-08-10 07:05:30 +0900

    feat: Use Font Awesome with npm package and webpack build

---
+++ assets/js/font-awesome.js
@@ -0,0 +1,3 @@
+'use strict'
+
+import '@fortawesome/fontawesome-free/css/all.min.css'
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+const recursiveIssuer = m => {
+  if (m.issuer) {
+    return recursiveIssuer(m.issuer)
+  } else if (m.name) {
+    return m.name
+  } else {
+    return false
+  }
+}
 
 module.exports = (env, argv) => {
-  const isModeNone = argv.mode === 'none'
   const isModeDev = argv.mode === 'development'
-  const mode = argv.mode
 
   let entries
   if (isModeDev) {
@@ -12,19 +23,20 @@ module.exports = (env, argv) => {
     }
   } else {
     entries = {
-      scripts: ['babel-polyfill', './assets/js/cd-scripts.babel.js'],
-      hljs: './assets/js/hljs.js',
-      hljs_web: './assets/js/hljs_web.js',
-      'scripts+hljs': [
+      'js/min/scripts': ['babel-polyfill', './assets/js/cd-scripts.babel.js'],
+      'js/min/hljs': './assets/js/hljs.js',
+      'js/min/hljs_web': './assets/js/hljs_web.js',
+      'js/min/scripts+hljs': [
         'babel-polyfill',
         './assets/js/cd-scripts.babel.js',
         './assets/js/hljs.js'
       ],
-      'scripts+hljs_web': [
+      'js/min/scripts+hljs_web': [
         'babel-polyfill',
         './assets/js/cd-scripts.babel.js',
         './assets/js/hljs_web.js'
-      ]
+      ],
+      'fonts/fontawesome/font-awesome': './assets/js/font-awesome.js'
     }
   }
 
@@ -33,15 +45,42 @@ module.exports = (env, argv) => {
     entry: entries,
     output: {
       filename: '[name].js',
-      path:
-        mode === 'none'
-          ? path.join(__dirname, 'assets/js/unmin/')
-          : path.join(__dirname, 'assets/js/min/')
+      path: path.join(__dirname, 'assets/')
     },
-    devtool: mode === 'development' ? 'source-map' : 'none',
+    devtool: isModeDev ? 'source-map' : 'none',
     optimization: {
-      minimize: !isModeNone
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true
+        }),
+        new OptimizeCssAssetsPlugin({
+          assetNameRegExp: /\.css$/,
+          cssProcessor: require('cssnano'),
+          cssProcessorPluginOptions: {
+            preset: ['default', { discardComments: { removeAll: true } }]
+          }
+        })
+      ],
+      splitChunks: {
+        cacheGroups: {
+          fontAwesomeStyles: {
+            name: 'fonts/fontawesome/font-awesome',
+            test: (m, c, entry = 'foo') =>
+              m.constructor.name === 'CssModule' &&
+              recursiveIssuer(m) === entry,
+            chunks: 'all',
+            enforce: true
+          }
+        }
+      }
     },
+    plugins: [
+      new MiniCssExtractPlugin({
+        fileName: '[name].min.css'
+      })
+    ],
     module: {
       rules: [
         {
@@ -53,6 +92,30 @@ module.exports = (env, argv) => {
               options: {
                 presets: [['env', { modules: false }]],
                 plugins: 'transform-es2015-for-of'
+              }
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: './fonts/fontawesome/'
+              }
+            },
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/fontawesome/fonts/'
               }
             }
           ]
