26449caa6b47f78d0d4d0e4a24e883e1ec65553e
Toshihiro Kanai <i@miruc.co>
2018-08-19 23:03:24 +0900

    chore: Fix copy script

--- bin/copy.sh
+++ bin/copy.sh
@@ -12,10 +12,6 @@ mkdir -p dist/assets/html/
 mkdir -p dist/assets/js/
 mkdir -p dist/assets/js/min/
 mkdir -p dist/assets/fonts/
-mkdir -p dist/assets/fonts/fontawesome/
-mkdir -p dist/assets/fonts/fontawesome/fonts/
-mkdir -p dist/assets/fonts/simple-icons/
-mkdir -p dist/assets/fonts/simple-icons/fonts
 
 cp *.php                    dist/
 cp *.css                    dist/
@@ -28,13 +24,10 @@ cp parts/czr/*.php          dist/parts/czr/
 cp page-templates/*.php     dist/page-templates/
 cp languages/coldbox.pot    dist/languages/
 
-cp assets/img/*.*                              dist/assets/img/
-cp -r assets/fonts/fontawesome/fonts/          dist/assets/fonts/fontawesome/
-cp assets/fonts/fontawesome/font-awesome.css   dist/assets/fonts/fontawesome/
-cp -r assets/fonts/simple-icons/fonts/         dist/assets/fonts/simple-icons/
-cp assets/js/min/*.js                          dist/assets/js/min/
-cp assets/js/*.js                              dist/assets/js/
-cp assets/css/*.min.css                        dist/assets/css/
-cp assets/html/*.html                          dist/assets/html/
+cp assets/img/*.*       dist/assets/img/
+cp assets/fonts/*.*     dist/assets/fonts/
+cp assets/js/*.js       dist/assets/js/
+cp assets/css/*.min.css dist/assets/css/
+cp assets/html/*.html   dist/assets/html/
 
 rm dist/assets/js/czr-scripts.babel.js
