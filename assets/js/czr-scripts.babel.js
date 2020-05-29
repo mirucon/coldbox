a71221508bf9429cdd133cd0d01a1a99b1db9ecc
Toshihiro Kanai <i@miruc.co>
2020-05-29 10:42:19 +0900

    style: Auto-fix styling as per Prettier 2.0

--- assets/js/czr-scripts.babel.js
+++ assets/js/czr-scripts.babel.js
@@ -1,11 +1,11 @@
 /* eslint no-undef: 0 */
 
-let czr = () => {
+const czr = () => {
   const _api = wp.customize
 
   // Shows concatenated JS files option when hljs option checked.
-  let hljs = _api.control('does_use_hljs').setting._value
-  let hljsWeb = _api.control('use_hljs_web_pack').setting._value
+  const hljs = _api.control('does_use_hljs').setting._value
+  const hljsWeb = _api.control('use_hljs_web_pack').setting._value
 
   if (!hljs && !hljsWeb) {
     _api.control('concat_js').deactivate()
@@ -14,7 +14,7 @@ let czr = () => {
   }
 
   // Shows Use Narrower Padding when Scrolling option when Horizontal selected.
-  let headerDirection = _api.control('header_direction').setting._value
+  const headerDirection = _api.control('header_direction').setting._value
 
   if (headerDirection !== 'row') {
     _api.control('use_narrower_padding').deactivate()
