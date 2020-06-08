1a59c618aedaeb0eedeb9aa9b7b387fb3bf3ee51
Toshihiro <i@miruc.co>
2020-06-08 08:18:44 +0000

    Update from travis c7c21f422d55f8f646b3a86b6823cdc9aca6d1ac

--- assets/js/simple-icons.font.js
+++ assets/js/simple-icons.font.js
@@ -1,11 +1,11 @@
 module.exports = {
   files: [
     '../../node_modules/simple-icons/icons/feedly.svg',
-    '../../node_modules/simple-icons/icons/hatenabookmark.svg'
+    '../../node_modules/simple-icons/icons/hatenabookmark.svg',
   ],
   fontName: 'simple-icons',
   classPrefix: 'si-',
   baseSelector: '.si',
   types: ['eot', 'woff', 'woff2', 'ttf'],
-  fileName: './fonts/[fontname].[ext]'
+  fileName: './fonts/[fontname].[ext]',
 }
