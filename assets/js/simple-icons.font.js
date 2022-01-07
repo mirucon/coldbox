2f5a6cf5044c1c1f77fead1ae97f59fbe0f1e506
Toshihiro Kanai <mirucon@users.noreply.github.com>
2022-01-07 10:55:55 +0000

    Update from travis

--- assets/js/simple-icons.font.js
+++ assets/js/simple-icons.font.js
@@ -1,12 +1,15 @@
+const path = require('path')
+
 module.exports = {
   files: [
-    '../../node_modules/simple-icons/icons/feedly.svg',
-    '../../node_modules/simple-icons/icons/hatenabookmark.svg',
+    path.resolve(__dirname, '../../node_modules/simple-icons/icons/feedly.svg'),
+    path.resolve(__dirname, '../../node_modules/simple-icons/icons/hatenabookmark.svg'),
   ],
   fontName: 'simple-icons',
   classPrefix: 'si-',
   baseSelector: '.si',
   types: ['eot', 'woff', 'woff2', 'ttf'],
+  // Path from `assets` (Specified in webpack.config.js)
   fileName: './fonts/[fontname].[ext]',
   cssTemplate: 'webfonts-generator-template.hbs',
 }
